27dfa7095cad0fc674ecbfea357f339cd2801d33
Felipe Martins <felipemartins96@gmail.com>
2020-08-11 22:49:36 -0300

    Added simple grSim client with send and receive capabilities

---
+++ grSimClient.py
@@ -0,0 +1,49 @@
+import socket
+# import binascii
+import pb.messages_robocup_ssl_wrapper_pb2 as wrapper_pb2
+import pb.grSim_Packet_pb2 as packet_pb2
+
+
+class grSimClient:
+
+    def __init__(self, ip='127.0.0.1', visionPort=10020, commandPort=20011):
+        """Init grSimClient object."""
+
+        self.ip = ip
+        self.visionPort = visionPort
+        self.commandPort = commandPort
+
+        # Connect vision and command sockets
+        self.visionSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.commandSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.visionSocket.bind((self.ip, self.visionPort))
+        self.commandAddress = (self.ip, self.commandPort)
+
+    def send(self, vx, vy, omega):
+        """Sends vx, vy and omega command to id = 0 blue robot"""
+        packet = packet_pb2.grSim_Packet()
+        grSimCommands = packet.commands
+        grSimRobotCommand = grSimCommands.robot_commands
+
+        grSimCommands.timestamp = 0.0
+        grSimCommands.isteamyellow = False
+        robot = grSimRobotCommand.add()
+        robot.id = 0
+        robot.kickspeedx = 0
+        robot.kickspeedz = 0
+        robot.veltangent = vx
+        robot.velnormal = vy
+        robot.velangular = omega
+        robot.spinner = False
+        robot.wheelsspeed = False
+
+        data = packet.SerializeToString()
+        self.commandSocket.sendto(data, self.commandAddress)
+
+    def receive(self):
+        """Receive SSL wrapper package and decode."""
+
+        data, _ = self.visionSocket.recvfrom(1024)
+        decoded_data = wrapper_pb2.SSL_WrapperPacket().FromString(data)
+
+        return decoded_data
