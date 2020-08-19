c994019d34f022dfcadb28da32ba3c191d58586f
Lucas Cavalcanti <lhcs@cin.ufpe.br>
2020-08-18 22:44:48 -0300

    Training ShootGoalie like Article (missing replay memory)

--- gym_ssl/grsim_ssl/shootGoalieEnv.py
+++ gym_ssl/grsim_ssl/shootGoalieEnv.py
@@ -9,20 +9,18 @@ from gym_ssl.grsim_ssl.Communication.grSimClient import grSimClient
 from gym_ssl.grsim_ssl.Entities.Ball import Ball
 from gym_ssl.grsim_ssl.Entities.Robot import Robot
 from gym_ssl.grsim_ssl.Entities.Frame import Frame
-from gym_ssl.grsim_ssl.goalieState import GoalieState
+from gym_ssl.grsim_ssl.utils.goalieState import GoalieState
+from gym_ssl.grsim_ssl.utils.util import *
 
 
 class shootGoalieEnv(GrSimSSLEnv):
   """
   Using cartpole env description as base example for our documentation
   Description:
-      A pole is attached by an un-actuated joint to a cart, which moves along
-      a frictionless track. The pendulum starts upright, and the goal is to
-      prevent it from falling over by increasing and reducing the cart's
-      velocity.
+      # TODO
   Source:
-      This environment corresponds to the version of the cart-pole problem
-      described by Barto, Sutton, and Anderson
+      # TODO
+
   Observation:
       Type: Box(16)
       Num     Observation                                       Min                     Max
@@ -38,8 +36,8 @@ class shootGoalieEnv(GrSimSSLEnv):
       9       Angle between blue id 0 and goal right (rad)    -math.pi                math.pi
       10      Angle between blue id 0 and goalie center(rad)  -math.pi                math.pi
       11      Angle between blue id 0 and goalie center(rad)  -math.pi                math.pi
-      12       Angle between blue id 0 and goalie left (rad)   -math.pi                math.pi
-      13       Angle between blue id 0 and goalie left (rad)   -math.pi                math.pi
+      12      Angle between blue id 0 and goalie left (rad)   -math.pi                math.pi
+      13      Angle between blue id 0 and goalie left (rad)   -math.pi                math.pi
       14      Angle between blue id 0 and goalie right (rad)  -math.pi                math.pi
       15      Angle between blue id 0 and goalie right (rad)  -math.pi                math.pi
 
@@ -47,27 +45,16 @@ class shootGoalieEnv(GrSimSSLEnv):
       Type: Box(2)
       Num     Action                        Min                     Max
       0       Blue id 0 Vw (rad/s)        -math.pi * 3            math.pi * 3
-      1       Blue Kick Strength (m/s)          -6.5                   6.5 <-------------------- VERIFY!!!!
-
-      Note: The amount the velocity that is reduced or increased is not
-      fixed; it depends on the angle the pole is pointing. This is because
-      the center of gravity of the pole increases the amount of energy needed
-      to move the cart underneath it
+      1       Blue Kick Strength (m/s)          -6.5                   6.5
   Reward:
       Reward is 1 for success, -1 to fails. 0 otherwise.
 
   Starting State:
       All observations are assigned a uniform random value in [-0.05..0.05]
-      # Algulo aleatorio na posicao do penalty
+      # TODO
 
   Episode Termination:
-      Pole Angle is more than 12 degrees.
-      Cart Position is more than 2.4 (center of the cart reaches the edge of
-      the display).
-      Episode length is greater than 200.
-      Solved Requirements:
-      Considered solved when the average return is greater than or equal to
-      195.0 over 100 consecutive trials.
+    # TODO
   """
   def __init__(self):
     super().__init__()
@@ -84,6 +71,7 @@ class shootGoalieEnv(GrSimSSLEnv):
   def _getCommands(self, actions):
     commands = []
     cmdAttacker = Robot(id=0, yellow=False, vw=actions[0], kickVx=actions[1], dribbler=True)
+    # cmdAttacker = Robot(id=0, yellow=False, vw=0, kickVx=0, dribbler=True)
     
     commands.append(cmdAttacker)
 
@@ -104,7 +92,7 @@ class shootGoalieEnv(GrSimSSLEnv):
 
   def _getFormation(self):
     # ball penalty position
-    ball = Ball(x=-4.2, y=0, vx=1, vy=0)
+    ball = Ball(x=-4.1, y=0, vx=0, vy=0)
     
     # Goalkeeper penalty position
     goalKeeper = Robot(id=0, x=-6, y=0, w=0, yellow = True)
@@ -119,19 +107,19 @@ class shootGoalieEnv(GrSimSSLEnv):
     done = False
 
     if self.state.ball.x < -6000:
-        # the ball out the field limits
-        done = True
-        if self.state.ball.y < 600 and self.state.ball.y > -600:
-            # ball entered the goal
-            reward = 1
-        else:
-            # the ball went out the bottom line
-            reward = -1
-
-    if self.state.ball.x < -5000:
+      # the ball out the field limits
+      done = True
+      if self.state.ball.y < 600 and self.state.ball.y > -600:
+          # ball entered the goal
+          reward = 1
+      else:
+          # the ball went out the bottom line
+          reward = -1
+    elif self.state.ball.x < -5000 and self.state.ball.vx > -1:
         # goalkeeper caught the ball
-        if self.state.ball.vx > -1:
-            done = True
-            reward = -1
-
+      done = True
+      reward = -1
+    elif mod(self.state.ball.vx, self.state.ball.vy) < 10: # 1 cm/s
+      done = True
+      reward = -1
     return reward, done
