dd64690eade4eea96306f34980260ae3a724ab45
goncamateus <mgm4@cin.ufpe.br>
2021-01-06 09:47:50 -0300

    removes ptan env iteration

--- envs/agents/deepvss/lib/common.py
+++ envs/agents/deepvss/lib/common.py
@@ -526,3 +526,38 @@ class PersistentExperiencePrioritizedReplayBuffer(ptan.experience.PrioritizedRep
             self._it_min[idx] = priority ** self._alpha
 
             self._max_priority = max(self._max_priority, priority)
+
+
+# Ornstein-Uhlenbeck process
+# Adding time-correlated noise to the actions taken by the deterministic policy
+# https://github.com/vitchyr/rlkit/blob/master/rlkit/exploration_strategies/ou_strategy.py
+
+class OUNoise(object):
+    def __init__(self, mu=0.0, theta=0.15,
+                 max_sigma=0.2, min_sigma=0.2, decay_period=9375):
+        self.mu = mu
+        self.theta = theta
+        self.sigma = max_sigma
+        self.max_sigma = max_sigma
+        self.min_sigma = min_sigma
+        self.decay_period = decay_period
+        self.action_dim = 2
+        self.low = -1
+        self.high = 1
+        self.reset()
+
+    def reset(self):
+        self.state = np.ones(self.action_dim) * self.mu
+
+    def evolve_state(self):
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * \
+            np.random.randn(self.action_dim)
+        self.state = x + dx
+        return self.state
+
+    def get_action(self, action, t=0):
+        ou_state = self.evolve_state()
+        self.sigma = self.max_sigma - \
+            (self.max_sigma - self.min_sigma) * min(1.0, t / self.decay_period)
+        return np.clip(action + ou_state, self.low, self.high)
--- envs/agents/deepvss/lib/ddpg_model.py
+++ envs/agents/deepvss/lib/ddpg_model.py
@@ -1,12 +1,14 @@
+import numpy as np
+import ptan
 import torch
 import torch.nn as nn
-import ptan
-from torch.autograd import Variable
 import torch.nn.functional as F
-import numpy as np
+
+from .common import OUNoise
 
 DDPG_MODELS_ACTOR = {}  # add new models to the dict after class definition
 DDPG_MODELS_CRITIC = {}  # add new models to the dict after class definition
+
 
 class DDPGActor(nn.Module):
     def __init__(self, obs_size, act_size):
@@ -44,45 +46,32 @@ class DDPGCritic(nn.Module):
         obs = self.obs_net(x)
         return self.out_net(torch.cat([obs, a], dim=1))
 
+
 DDPG_MODELS_ACTOR["linear"] = DDPGActor
 
 DDPG_MODELS_CRITIC["linear"] = DDPGCritic
+
 
 class AgentDDPG(ptan.agent.BaseAgent):
     """
     Agent implementing Orstein-Uhlenbeck exploration process
     """
-    def __init__(self, net, device="cpu", ou_enabled=True, ou_mu=0.0, ou_teta=0.15, ou_sigma=0.2):
+
+    def __init__(self, net, device="cpu", ou_enabled=True,
+                 ou_mu=0.0, ou_teta=0.15, ou_sigma=0.2):
         self.net = net
         self.device = device
         self.ou_enabled = ou_enabled
-        self.ou_mu = ou_mu
-        self.ou_teta = ou_teta
-        self.ou_sigma = ou_sigma
-        self.ou_epsilon = 1.0
+        self.ou_noise = OUNoise(mu=ou_mu, theta=ou_teta,
+                                max_sigma=ou_sigma, min_sigma=ou_sigma)
 
     def initial_state(self):
         return None
 
-    def __call__(self, states, agent_states):
-        self.ou_epsilon = 1.0#self.selector.epsilon
-        #self.ou_epsilon = np.max((self.ou_epsilon*self.ou_epsilon_decay, 0.2))
-
+    def __call__(self, states, n_step):
         states_v = ptan.agent.float32_preprocessor(states).to(self.device)
         mu_v = self.net(states_v)
         actions = mu_v.data.cpu().numpy()
-
-        if self.ou_enabled and self.ou_epsilon > 0:
-            new_a_states = []
-            for a_state, action in zip(agent_states, actions):
-                if a_state is None:
-                    a_state = np.zeros(shape=action.shape, dtype=np.float32)
-                a_state += self.ou_teta * (self.ou_mu - a_state)
-                a_state += self.ou_sigma * np.random.normal(size=action.shape)
-
-                action += self.ou_epsilon * a_state
-                new_a_states.append(a_state)
-        else:
-            new_a_states = agent_states
-        actions = np.clip(actions, -1, 1)
-        return actions, new_a_states
+        if self.ou_enabled:
+            actions = self.ou_noise.get_action(actions, n_step)
+        return actions
