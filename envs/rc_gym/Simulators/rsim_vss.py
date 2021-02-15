7d3b5c9ef490b3080b2a3c1a6328dc51488eb3d7
thurcst <arthurhenriqueanibal@gmail.com>
2021-02-14 22:44:40 -0300

    Fira done

--- envs/rc_gym/Simulators/rsim_vss.py
+++ envs/rc_gym/Simulators/rsim_vss.py
@@ -1,22 +1,33 @@
-import robosim
-import numpy as np
 from typing import Dict, List
+
+import numpy as np
+import robosim
 from rc_gym.Entities import FrameVSS
 from rc_gym.Simulators.rsim_base import RSim
 
 
 class RSimVSS(RSim):
-    def __init__(self, field_type: int, n_robots_blue: int,
-                 n_robots_yellow: int, time_step_ms: int):
-        super().__init__(field_type=field_type, n_robots_blue=n_robots_blue,
-                         n_robots_yellow=n_robots_yellow, time_step_ms=time_step_ms)
+    def __init__(
+        self,
+        field_type: int,
+        n_robots_blue: int,
+        n_robots_yellow: int,
+        time_step_ms: int,
+    ):
+        super().__init__(
+            field_type=field_type,
+            n_robots_blue=n_robots_blue,
+            n_robots_yellow=n_robots_yellow,
+            time_step_ms=time_step_ms,
+        )
 
         self.linear_speed_range = 1.15  # m/s
         self.robot_wheel_radius = 0.026
 
     def send_commands(self, commands):
         sim_commands = np.zeros(
-            (self.n_robots_blue + self.n_robots_yellow, 2), dtype=np.float64)
+            (self.n_robots_blue + self.n_robots_yellow, 2), dtype=np.float64
+        )
 
         for cmd in commands:
             if cmd.yellow:
@@ -36,9 +47,16 @@ class RSimVSS(RSim):
 
         return frame
 
-    def _init_simulator(self, field_type, n_robots_blue, n_robots_yellow,
-                        ball_pos, blue_robots_pos, yellow_robots_pos,
-                        time_step_ms):
+    def _init_simulator(
+        self,
+        field_type,
+        n_robots_blue,
+        n_robots_yellow,
+        ball_pos,
+        blue_robots_pos,
+        yellow_robots_pos,
+        time_step_ms,
+    ):
 
         return robosim.SimulatorVSS(
             field_type=field_type,
@@ -47,5 +65,5 @@ class RSimVSS(RSim):
             ball_pos=ball_pos,
             blue_robots_pos=blue_robots_pos,
             yellow_robots_pos=yellow_robots_pos,
-            time_step_ms=time_step_ms
-            )
+            time_step_ms=time_step_ms,
+        )
