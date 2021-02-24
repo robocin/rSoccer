51973d7880ee2d8497c561fc13de89a35ae41162
goncamateus <mgm4@cin.ufpe.br>
2021-02-23 21:12:58 -0300

    Fixes imports

--- envs/rc_gym/vss/__init__.py
+++ envs/rc_gym/vss/__init__.py
@@ -1,2 +1,2 @@
 from rc_gym.vss.env_3v3.vss_gym_3v3 import VSS3v3Env
-from rc_gym.vss.env_ma.vss_gym_ma import VSSMADef, VSSMAEnv, VSSMAOpp
+from rc_gym.vss.env_ma.vss_gym_ma import VSSMAEnv, VSSMAOpp
