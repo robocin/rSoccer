d03f627505112f61315120094d456ccc71bda3f5
Felipe <felipemartins96@gmail.com>
2021-03-04 10:21:31 -0300

    limit ssl linear speed to same value for all directions

--- envs/rc_gym/Simulators/rsim.py
+++ envs/rc_gym/Simulators/rsim.py
@@ -154,8 +154,9 @@ class RSimSSL(RSim):
             else:
                 rbt_id = cmd.id
             # convert from linear speed to angular speed
-            sim_commands[rbt_id][0] = cmd.v_x
-            sim_commands[rbt_id][1] = cmd.v_y
+            v_x, v_y = self._convert_speed(cmd.v_x, cmd.v_y)
+            sim_commands[rbt_id][0] = v_x
+            sim_commands[rbt_id][1] = v_y
             # TODO checar se a velocidade é rad ou deg/s
             sim_commands[rbt_id][2] = cmd.v_theta * self.angular_speed_range
             sim_commands[rbt_id][3] = cmd.kick_v_x
@@ -172,6 +173,11 @@ class RSimSSL(RSim):
 
         return frame
 
+    def _convert_speed(self, v_x, v_y):
+        v_norm = np.linalg.norm([v_x,v_y])
+        c = v_norm < self.linear_speed_range or self.linear_speed_range / v_norm
+        return v_x*c, v_y*c
+    
     def _init_simulator(self, field_type, n_robots_blue, n_robots_yellow,
                         ball_pos, blue_robots_pos, yellow_robots_pos,
                         time_step_ms):
