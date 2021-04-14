5e672a63f2451004d628d77e8204998fcadf2ca8
Felipe <felipemartins96@gmail.com>
2021-04-14 14:46:48 -0300

    add ssl contested possession rewards

--- envs/rc_gym/ssl/ssl_hw_challenge/contested_possession.py
+++ envs/rc_gym/ssl/ssl_hw_challenge/contested_possession.py
@@ -46,13 +46,30 @@ class SSLContestedPossessionEnv(SSLBaseEnv):
         self.action_space = gym.spaces.Box(low=-1, high=1,
                                            shape=(5, ), dtype=np.float32)
         
-        n_obs = 4 + 7*self.n_robots_blue + 5*self.n_robots_yellow
+        n_obs = 4 + 8*self.n_robots_blue + 2*self.n_robots_yellow
         self.observation_space = gym.spaces.Box(low=-self.NORM_BOUNDS,
                                                 high=self.NORM_BOUNDS,
                                                 shape=(n_obs, ),
                                                 dtype=np.float32)
 
+        # scale max dist rw to 1 Considering that max possible move rw if ball and robot are in opposite corners of field
+        self.ball_dist_scale = np.linalg.norm([self.field.width, self.field.length/2])
+        self.ball_grad_scale = np.linalg.norm([self.field.width/2, self.field.length/2])/4
+        
+        # scale max energy rw to 1 Considering that max possible energy if max robot wheel speed sent every step
+        wheel_max_rad_s = 160
+        max_steps = 1200
+        self.energy_scale = ((wheel_max_rad_s * 4) * max_steps)
+
         print('Environment initialized')
+
+    def reset(self):
+        self.reward_shaping_total = None
+        return super().reset()
+
+    def step(self, action):
+        observation, reward, done, _ = super().step(action)
+        return observation, reward, done, self.reward_shaping_total
 
     def _frame_to_observations(self):
 
@@ -75,6 +92,7 @@ class SSLContestedPossessionEnv(SSLBaseEnv):
             observation.append(self.norm_v(self.frame.robots_blue[i].v_x))
             observation.append(self.norm_v(self.frame.robots_blue[i].v_y))
             observation.append(self.norm_w(self.frame.robots_blue[i].v_theta))
+            observation.append(1 if self.frame.robots_blue[i].infrared else 0)
 
         for i in range(self.n_robots_yellow):
             observation.append(self.norm_pos(self.frame.robots_yellow[i].x))
@@ -95,6 +113,18 @@ class SSLContestedPossessionEnv(SSLBaseEnv):
         return commands
 
     def _calculate_reward_and_done(self):
+        if self.reward_shaping_total is None:
+            self.reward_shaping_total = {
+                'goal': 0,
+                'rbt_in_gk_area': 0,
+                'done_ball_out': 0,
+                'done_ball_out_right': 0,
+                'done_rbt_out': 0,
+                'ball_dist': 0,
+                'ball_grad': 0,
+                'energy': 0,
+                'collision': 0
+            }
         reward = 0
         done = False
         
@@ -114,25 +144,48 @@ class SSLContestedPossessionEnv(SSLBaseEnv):
         # End episode in case of collision
         for rbt in self.frame.robots_yellow.values():
             if abs(rbt.v_x) > 0.1 or abs(rbt.v_y) > 0.1:
+                self.reward_shaping_total['collision'] += 1
                 done = True
         
-        # Check if robot exited right field side limits
-        if robot.x < 0 or abs(robot.y) > half_wid:
+        # Check if robot exited field right side limits
+        if robot.x < -0.2 or abs(robot.y) > half_wid:
             done = True
+            self.reward_shaping_total['done_rbt_out'] += 1
         # If flag is set, end episode if robot enter gk area
         elif robot_in_gk_area(robot):
             done = True
+            self.reward_shaping_total['rbt_in_gk_area'] += 1
         # Check ball for ending conditions
         elif ball.x < 0 or abs(ball.y) > half_wid:
             done = True
+            self.reward_shaping_total['done_ball_out'] += 1
         elif ball.x > half_len:
             done = True
-            reward = 1 if abs(ball.y) < half_goal_wid else 0
+            if abs(ball.y) < half_goal_wid:
+                reward = 5 
+                self.reward_shaping_total['goal'] += 1
+            else:
+                reward = 0
+                self.reward_shaping_total['done_ball_out_right'] += 1
+        elif self.last_frame is not None:
+            ball_dist_rw = self.__ball_dist_rw() / self.ball_dist_scale
+            self.reward_shaping_total['ball_dist'] += ball_dist_rw
+            
+            ball_grad_rw = self.__ball_grad_rw() / self.ball_grad_scale
+            self.reward_shaping_total['ball_grad'] += ball_grad_rw
+            
+            energy_rw = -self.__energy_pen() / self.energy_scale
+            self.reward_shaping_total['energy'] += energy_rw
+            
+            reward = reward\
+                    + ball_dist_rw\
+                    + ball_grad_rw\
+                    + energy_rw
 
         done = done
 
         return reward, done
-    
+
     def _get_initial_positions_frame(self):
         '''Returns the position of each robot and ball for the initial frame'''
         half_len = self.field.length / 2
@@ -149,11 +202,77 @@ class SSLContestedPossessionEnv(SSLBaseEnv):
             enemy_y = y()
         else:
             pos_frame.robots_blue[0] = Robot(x=0, y=0, theta=0.)
-            enemy_x = half_len - pen_len
-            enemy_y = 0.
+            enemy_x = x()
+            enemy_y = y()
 
 
         pos_frame.ball = Ball(x=enemy_x-0.1, y=enemy_y)
         pos_frame.robots_yellow[0] = Robot(x=enemy_x, y=enemy_y, theta=180.)
 
         return pos_frame
+
+    def __ball_dist_rw(self):
+        assert(self.last_frame is not None)
+        
+        # Calculate previous ball dist
+        last_ball = self.last_frame.ball
+        last_robot = self.last_frame.robots_blue[0]
+        last_ball_pos = np.array([last_ball.x, last_ball.y])
+        last_robot_pos = np.array([last_robot.x, last_robot.y])
+        last_ball_dist = np.linalg.norm(last_robot_pos - last_ball_pos)
+        
+        # Calculate new ball dist
+        ball = self.frame.ball
+        robot = self.frame.robots_blue[0]
+        ball_pos = np.array([ball.x, ball.y])
+        robot_pos = np.array([robot.x, robot.y])
+        ball_dist = np.linalg.norm(robot_pos - ball_pos)
+        
+        ball_dist_rw = last_ball_dist - ball_dist
+        
+        if ball_dist_rw > 1:
+            print("ball_dist -> ", ball_dist_rw)
+            print(self.frame.ball)
+            print(self.frame.robots_blue)
+            print(self.frame.robots_yellow)
+            print("===============================")
+        
+        return np.clip(ball_dist_rw, -1, 1)
+
+    def __ball_grad_rw(self):
+        assert(self.last_frame is not None)
+        
+        # Goal pos
+        goal = np.array([self.field.length/2, 0.])
+        
+        # Calculate previous ball dist
+        last_ball = self.last_frame.ball
+        ball = self.frame.ball
+        last_ball_pos = np.array([last_ball.x, last_ball.y])
+        last_ball_dist = np.linalg.norm(goal - last_ball_pos)
+        
+        # Calculate new ball dist
+        ball_pos = np.array([ball.x, ball.y])
+        ball_dist = np.linalg.norm(goal - ball_pos)
+        
+        ball_dist_rw = last_ball_dist - ball_dist
+        
+        if ball_dist_rw > 1:
+            print("ball_dist -> ", ball_dist_rw)
+            print(self.frame.ball)
+            print(self.frame.robots_blue)
+            print(self.frame.robots_yellow)
+            print("===============================")
+        
+        return np.clip(ball_dist_rw, -1, 1)
+
+    def __energy_pen(self):
+        robot = self.frame.robots_blue[0]
+        
+        # Sum of abs each wheel speed sent
+        energy = abs(robot.v_wheel0)\
+            + abs(robot.v_wheel1)\
+            + abs(robot.v_wheel2)\
+            + abs(robot.v_wheel3)
+            
+        return energy
