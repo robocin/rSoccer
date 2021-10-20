3105d5141d3fa244d41351dea6325f3ce2977aac
Felipe Martins <felipemartins96@gmail.com>
2021-10-19 21:58:14 -0300

    Make package installable
    testing add init to vss folder
    
    
    add protobuf as requirement and init ssl module

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 '''
 #       Install Project Requirements 
 '''
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='rsoccer_gym',
     version='0.1',
-    install_requires=['gym', 'protobuf', 'sympy']
+    packages=[package for package in find_packages() if package.startswith("rsoccer_gym")],
+    install_requires=['gym', 'rc-robosim', 'pyglet', 'protobuf']
 )
