4e8180ba860277d4f651e060b4096fe4618b4388
Felipe Martins <felipemartins96@gmail.com>
2021-10-20 13:35:39 -0300

    bump to 0.1.0a4: add fixed rSim version requirement
    fix robosim version

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@
 from setuptools import setup, find_packages
 
 setup(name='rsoccer-gym',
-    version='0.1.0a3',
+    version='0.1.0a4',
     url="https://github.com/robocin/rSoccer",
     description="SSL and VSS robot soccer gym environments",
     packages=[package for package in find_packages() if package.startswith("rsoccer_gym")],
-    install_requires=['gym', 'rc-robosim', 'pyglet', 'protobuf']
+    install_requires=['gym', 'rc-robosim==0.0.8a0', 'pyglet', 'protobuf']
 )
