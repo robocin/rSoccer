18aeb01b3c8d85a1bc596e07d78efdefa843d6b4
Felipe Martins <felipemartins96@gmail.com>
2021-10-19 22:10:25 -0300

    fix package import error

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@
 '''
 from setuptools import setup, find_packages
 
-setup(name='rsoccer_gym',
-    version='0.1',
+setup(name='rsoccer-gym',
+    version='0.1.0a2',
+    url="https://github.com/robocin/rSoccer",
+    description="SSL and VSS robot soccer gym environments",
     packages=[package for package in find_packages() if package.startswith("rsoccer_gym")],
     install_requires=['gym', 'rc-robosim', 'pyglet', 'protobuf']
 )
