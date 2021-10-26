927031b44e6a37289e48249cf2692bdb19b7a849
Felipe Martins <felipemartins96@gmail.com>
2021-10-26 12:43:46 -0300

    feat(Package): added setuptools_scm package version management

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@
 from setuptools import setup, find_packages
 
 setup(name='rsoccer-gym',
-    version='0.1.0a4',
     url="https://github.com/robocin/rSoccer",
     description="SSL and VSS robot soccer gym environments",
     packages=[package for package in find_packages() if package.startswith("rsoccer_gym")],
