b521819bca1438c6ee24d16aaee1dd799d8660de
goncamateus <mgm4@cin.ufpe.br>
2022-03-25 16:22:54 -0300

    :construction: Quick fix to fix rendering

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='rsoccer-gym',
     url="https://github.com/robocin/rSoccer",
     description="SSL and VSS robot soccer gym environments",
     packages=[package for package in find_packages() if package.startswith("rsoccer_gym")],
-    install_requires=['gym', 'rc-robosim>=1.2.0', 'pyglet', 'protobuf']
+    install_requires=['gym==0.21.0', 'rc-robosim>=1.2.0', 'pyglet', 'protobuf']
 )
