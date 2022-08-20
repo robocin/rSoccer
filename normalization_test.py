1a54ef586e7c66a4fef0bde6f20f354fcc224282
Icaro-Nunes <icarolimanunes@gmail.com>
2022-08-20 20:53:36 -0300

    Emptied experiment information on normalization_test and put in use 'VSS5v5-v0' registration

--- normalization_test.py
+++ normalization_test.py
@@ -1,13 +1,11 @@
-# This is the experiment made to obtain the maximum accumulated energy_penalty in
-# the VSS5v5Env. There's only one robot in the 5v5 field, which will 
-# be trained without the influence of energy_penalty
+# No experiment being run
 
 import gym
-from rsoccer_gym.vss.env_vss.vss5v5_gym import VSS5v5Env
+import rsoccer_gym
 
 debug = dict()
 
-env = VSS5v5Env()
+env = gym.make("VSS5v5-v0")
 state = env.reset()
 
 observation_size = env.observation_space.shape[0]
@@ -24,6 +22,3 @@ if __name__ == '__main__':
         env.render()
     
     print(info)
-
-# The found maximum accumulated energy_penalty was 30000
-# experiment: https://wandb.ai/robocin/RoboCIn-RL/runs/4vdd7if2?workspace=user-icaro-nunes
