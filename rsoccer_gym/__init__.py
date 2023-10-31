3bde0efe3447a08fc8d3ce20c645c15d6b954914
Felipe Martins <felipemartins96@gmail.com>
2023-10-31 16:53:16 -0300

    remove non hardware challenge ssl environments

--- rsoccer_gym/__init__.py
+++ rsoccer_gym/__init__.py
@@ -5,25 +5,6 @@ register(id='VSS-v0',
          max_episode_steps=1200
          )
 
-register(id='SSLGoToBall-v0',
-         entry_point='rsoccer_gym.ssl.ssl_go_to_ball:SSLGoToBallEnv',
-         kwargs={'field_type': 2, 'n_robots_yellow': 6},
-         max_episode_steps=1200
-         )
-
-register(id='SSLGoToBallIR-v0',
-         entry_point='rsoccer_gym.ssl.ssl_go_to_ball:SSLGoToBallIREnv',
-         kwargs={'field_type': 2, 'n_robots_yellow': 6},
-         max_episode_steps=1200
-         )
-
-register(id='SSLGoToBallShoot-v0',
-         entry_point='rsoccer_gym.ssl.ssl_go_to_ball_shoot:SSLGoToBallShootEnv',
-         kwargs={'field_type': 2, 'random_init': True,
-                 'enter_goal_area': False},
-         max_episode_steps=2400
-         )
-
 register(id='SSLStaticDefenders-v0',
          entry_point='rsoccer_gym.ssl.ssl_hw_challenge.static_defenders:SSLHWStaticDefendersEnv',
          kwargs={'field_type': 2},
@@ -43,9 +24,4 @@ register(id='SSLContestedPossession-v0',
 register(id='SSLPassEndurance-v0',
          entry_point='rsoccer_gym.ssl.ssl_hw_challenge:SSLPassEnduranceEnv',
          max_episode_steps=120
-         )
-
-register(id='SSLPassEnduranceMA-v0',
-         entry_point='rsoccer_gym.ssl.ssl_hw_challenge:SSLPassEnduranceMAEnv',
-         max_episode_steps=1200
          )
