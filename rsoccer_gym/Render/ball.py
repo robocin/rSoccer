a4acdfbf17f222a3497125cf3164a1778ff3a8b1
goncamateus <mgm4@cin.ufpe.br>
2023-11-11 11:26:08 -0300

    Add ball rendering

---
+++ rsoccer_gym/Render/ball.py
@@ -0,0 +1,21 @@
+import pygame
+
+from utils import COLORS
+
+
+class Ball:
+    radius = 0.0215
+
+    def __init__(self, x, y, scale) -> None:
+        self.x = x
+        self.y = y
+        self.radius *= scale
+        self.scale = scale
+
+    def update(self, x, y):
+        self.x = x
+        self.y = y
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, COLORS["ORANGE"], (self.x, self.y), self.radius)
+        pygame.draw.circle(screen, COLORS["BLACK"], (self.x, self.y), self.radius, 1)
