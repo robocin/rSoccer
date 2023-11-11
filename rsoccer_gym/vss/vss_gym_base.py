053120fa722012f623314b8bfe75639f700b8d16
goncamateus <mgm4@cin.ufpe.br>
2023-11-11 15:12:06 -0300

    Refactor the setup

--- rsoccer_gym/vss/vss_gym_base.py
+++ rsoccer_gym/vss/vss_gym_base.py
@@ -1,8 +1,8 @@
-'''
+"""
 #   Environment Communication Structure
 #    - Father class that creates the structure to communicate with multples setups of enviroment
 #    - To create your wrapper from env to communcation, use inherit from this class! 
-'''
+"""
 
 
 import time
@@ -18,7 +18,6 @@ from rsoccer_gym.Simulators.rsim import RSimVSS
 from rsoccer_gym.Simulators.fira import Fira
 
 
-
 class VSSBaseEnv(gym.Env):
     metadata = {
         "render.modes": ["human", "rgb_array"],
@@ -27,35 +26,43 @@ class VSSBaseEnv(gym.Env):
         "render.fps": 60,
     }
     NORM_BOUNDS = 1.2
-    
-    def __init__(self, field_type: int,
-                 n_robots_blue: int, n_robots_yellow: int, time_step: float):
+
+    def __init__(
+        self,
+        field_type: int,
+        n_robots_blue: int,
+        n_robots_yellow: int,
+        time_step: float,
+    ):
         # Initialize Simulator
         self.time_step = time_step
-        self.rsim = RSimVSS(field_type=field_type,
-                                      n_robots_blue=n_robots_blue,
-                                      n_robots_yellow=n_robots_yellow,
-                                      time_step_ms=int(self.time_step*1000))
+        self.rsim = RSimVSS(
+            field_type=field_type,
+            n_robots_blue=n_robots_blue,
+            n_robots_yellow=n_robots_yellow,
+            time_step_ms=int(self.time_step * 1000),
+        )
         self.n_robots_blue = n_robots_blue
         self.n_robots_yellow = n_robots_yellow
 
         # Get field dimensions
         self.field_type = field_type
         self.field = self.rsim.get_field_params()
-        self.max_pos = max(self.field.width / 2, (self.field.length / 2) 
-                                + self.field.penalty_length)
+        self.max_pos = max(
+            self.field.width / 2, (self.field.length / 2) + self.field.penalty_length
+        )
         max_wheel_rad_s = (self.field.rbt_motor_max_rpm / 60) * 2 * np.pi
         self.max_v = max_wheel_rad_s * self.field.rbt_wheel_radius
         # 0.04 = robot radius (0.0375) + wheel thicknees (0.0025)
         self.max_w = np.rad2deg(self.max_v / 0.04)
-        
-        # Initiate 
+
+        # Initiate
         self.frame: Frame = None
         self.last_frame: Frame = None
         self.view = None
         self.steps = 0
         self.sent_commands = None
-        
+
         # Render
         self.field_renderer = VSSRenderField()
         self.window_surface = None
@@ -86,7 +93,7 @@ class VSSBaseEnv(gym.Env):
         self.sent_commands = None
 
         # Close render window
-        del(self.view)
+        del self.view
         self.view = None
 
         initial_pos_frame: Frame = self._get_initial_positions_frame()
@@ -96,7 +103,7 @@ class VSSBaseEnv(gym.Env):
         self.frame = self.rsim.get_frame()
 
         return self._frame_to_observations()
-    
+
     def _render(self):
         def pos_transform(pos_x, pos_y):
             return (
@@ -177,7 +184,6 @@ class VSSBaseEnv(gym.Env):
             return np.transpose(
                 np.array(pygame.surfarray.pixels3d(self.window_surface)), axes=(1, 0, 2)
             )
-        
 
     def close(self):
         if self.window_surface is not None:
@@ -188,46 +194,38 @@ class VSSBaseEnv(gym.Env):
         self.rsim.stop()
 
     def _get_commands(self, action):
-        '''returns a list of commands of type List[Robot] from type action_space action'''
+        """returns a list of commands of type List[Robot] from type action_space action"""
         raise NotImplementedError
 
     def _frame_to_observations(self):
-        '''returns a type observation_space observation from a type List[Robot] state'''
+        """returns a type observation_space observation from a type List[Robot] state"""
         raise NotImplementedError
 
     def _calculate_reward_and_done(self):
-        '''returns reward value and done flag from type List[Robot] state'''
+        """returns reward value and done flag from type List[Robot] state"""
         raise NotImplementedError
 
     def _get_initial_positions_frame(self) -> Frame:
-        '''returns frame with robots initial positions'''
+        """returns frame with robots initial positions"""
         raise NotImplementedError
 
     def norm_pos(self, pos):
-        return np.clip(
-            pos / self.max_pos,
-            -self.NORM_BOUNDS,
-            self.NORM_BOUNDS
-        )
+        return np.clip(pos / self.max_pos, -self.NORM_BOUNDS, self.NORM_BOUNDS)
 
     def norm_v(self, v):
-        return np.clip(
-            v / self.max_v,
-            -self.NORM_BOUNDS,
-            self.NORM_BOUNDS
-        )
+        return np.clip(v / self.max_v, -self.NORM_BOUNDS, self.NORM_BOUNDS)
 
     def norm_w(self, w):
-        return np.clip(
-            w / self.max_w,
-            -self.NORM_BOUNDS,
-            self.NORM_BOUNDS
-        )
+        return np.clip(w / self.max_w, -self.NORM_BOUNDS, self.NORM_BOUNDS)
 
 
 class VSSBaseFIRAEnv(VSSBaseEnv):
-
-    def __init__(self, field_type: int,
-                 n_robots_blue: int, n_robots_yellow: int, time_step: float):
+    def __init__(
+        self,
+        field_type: int,
+        n_robots_blue: int,
+        n_robots_yellow: int,
+        time_step: float,
+    ):
         super().__init__(field_type, n_robots_blue, n_robots_yellow, time_step)
         self.rsim = Fira()
--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
-'''
+"""
 #       Install Project Requirements 
-'''
+"""
 from setuptools import setup, find_packages
 
-setup(name='rsoccer-gym',
+setup(
+    name="rsoccer-gym",
     url="https://github.com/robocin/rSoccer",
     description="SSL and VSS robot soccer gym environments",
-    packages=[package for package in find_packages() if package.startswith("rsoccer_gym")],
-    install_requires=['gym==0.21.0', 'rc-robosim>=1.2.0', 'pyglet', 'protobuf']
+    packages=[
+        package for package in find_packages() if package.startswith("rsoccer_gym")
+    ],
+    install_requires=[
+        "gym == 0.21.0",
+        "rc-robosim >= 1.2.0",
+        "pygame >= 2.1.3",
+        "protobuf == 3.20",
+    ],
 )
