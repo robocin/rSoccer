c5b52a68975c44b1f9f775d8d920bb53de1a9611
goncamateus <mgm4@cin.ufpe.br>
2024-10-21 22:43:27 -0300

    Feat: Adding poetry to project

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ setup(
         package for package in find_packages() if package.startswith("rsoccer_gym")
     ],
     install_requires=[
-        "gymnasium >= 0.28.1",
+        "gymnasium >= 1.0.0",
         "rc-robosim >= 1.2.0",
-        "pygame >= 2.1.3",
-        "protobuf == 3.20",
+        "pygame >=2.1.0",
+        "protobuf == 3.20.2",
     ],
 )
